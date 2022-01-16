412638de03aa932bd5258bf5e7af3ab221c03fac
Cat Crusader <cat.crusader31@gmail.com>
2022-01-16 19:01:11 +0200

    add input

--- project/project/project.cpp
+++ project/project/project.cpp
@@ -1,11 +1,61 @@
 // project.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <windows.h>
+#include <stdio.h>
 #include <iostream>
+#include <tchar.h>
+#include <sstream>
+#include <conio.h>
 
+
+using namespace std;
+
+int enterNumber(const string& number)
+{
+    istringstream date_stream(number);
+    bool number_is_correct = true;
+    int new_number;
+    number_is_correct = number_is_correct && (date_stream >> new_number);
+    number_is_correct = number_is_correct && date_stream.eof();
+
+    if (!number_is_correct)
+    {
+        throw logic_error("Wrong number format: " + number);
+    }
+    return new_number;
+}
+
+int getParametrs()
+{
+    while (true)
+    {
+        cout << "Enter number" << endl;
+        string line;
+        getline(cin, line);
+        try
+        {
+            return enterNumber(line);
+        }
+        catch (exception e)
+        {
+            cout << e.what() << endl;
+            cout << "Try again." << endl << endl;
+        }
+    }
+}
 int main()
 {
     std::cout << "Hello World!\n";
+    while (true) {
+        cout << "Press any key to start program" << endl<<"Press esc to close program"<<endl;
+        if (_getch() == 27) {// ESC button, ends program
+            return 0;
+        }
+        else {
+
+        }
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
